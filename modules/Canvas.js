53673d613a7ceeee719361d5cfe0c7c6fac877aa
dada542 <45128986+dada542@users.noreply.github.com>
2020-06-22 01:02:09 +0200

    Update Canvas.js

--- modules/Canvas.js
+++ modules/Canvas.js
@@ -1,20 +1,20 @@
-export function createCanvas(parent=document.body,w=300,h=200) {
+export function createCanvas(parent=document.body,w=300,h=200,offset=[0,0]) {
   const c=document.createElement("canvas");
   c.width=w;
   c.height=h;
   c.style.position="fixed";
-  c.style.left=0;
-  c.style.top=0;
+  c.style.left=offset[0]+"px";
+  c.style.top=offset[1]+"px";
   parent.appendChild(c);
   const ctx=c.getContext("2d");
-  return [c,ctx,w,h];
+  return [c,ctx,w,h,offset];
 }
 export function fitScreen(c,r) {
   let [w,h]=[innerWidth,innerWidth/r];
   if(h>innerHeight) [w,h]=[innerHeight*r,innerHeight];
   c.width=w;
   c.height=h;
-  let offset=[(innerWidth-w)/2,(innerHeight-h)/2];
+  const offset=[(innerWidth-w)/2,(innerHeight-h)/2];
   c.style.left=offset[0]+"px";
   c.style.top=offset[1]+"px";
   return [w,h,offset];
